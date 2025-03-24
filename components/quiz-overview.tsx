f8ed0a7b3639502bd72cd0f144bec57b571c2bd7
moabzomg <wumantsun@yahoo.com.hk>
2025-03-24 19:16:21 +0000

    modify quiz overview

--- components/quiz-overview.tsx
+++ components/quiz-overview.tsx
@@ -11,7 +11,61 @@ export default function QuizReview({
   questions,
   userAnswers,
 }: QuizReviewProps) {
-  const answerLabels: ("A" | "B" | "C" | "D")[] = ["A", "B", "C", "D"];
+  const answerLabels: (
+    | "A"
+    | "B"
+    | "C"
+    | "D"
+    | "E"
+    | "F"
+    | "G"
+    | "H"
+    | "I"
+    | "J"
+    | "K"
+    | "L"
+    | "M"
+    | "N"
+    | "O"
+    | "P"
+    | "Q"
+    | "R"
+    | "S"
+    | "T"
+    | "U"
+    | "V"
+    | "W"
+    | "X"
+    | "Y"
+    | "Z"
+  )[] = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
 
   return (
     <Card className="w-full">
